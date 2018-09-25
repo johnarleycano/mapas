908522e55f1386ce73afc6301bf3eb774b35ff1d
John Arley <johnarleycano@hotmail.com>
2018-09-25 11:46:06 -0500

    refactor(Operaciones - Punto incidente): cambios generales

--- application/views/operaciones/incidentes/punto.php
+++ application/views/operaciones/incidentes/punto.php
@@ -27,66 +27,88 @@
 				// Consulta de los datos del incidente
 			    var incidente = ajax(`${$("#url").val()}/operaciones/obtener`, {"tipo": "incidente", "id":  this.id_incidente}, 'JSON')
 			    
-			    // Variables
-				var coordenadas = []
-			    var abscisa = parseInt(incidente.abscisa)
-			    var kilometro = Math.trunc(abscisa / 1000) * 1000
-			    var residuo_abscisa =  (abscisa % 1000) / 1000
+			    if(incidente){
+				    // Variables
+					var coordenadas = []
+				    var abscisa = parseInt(incidente.abscisa)
+				    var kilometro = Math.trunc(abscisa / 1000) * 1000
+				    var residuo_abscisa =  (abscisa % 1000) / 1000
 
-			    // Consulta de puntos del kilómetro
-			    var puntos_kilometro = ajax(`${$("#url").val()}/filtros/obtener`, {"tipo": "vias_geometria", "id":  {"id_sector": null, "id_via": incidente.id_via_configuracion, "kilometro": kilometro}}, 'JSON')
-			    
-			    // Si trae resultados
-			    if(puntos_kilometro.features[0]){
-			    	// Se almacena las coordenadas
-		       		let coordenadas_kilometro = puntos_kilometro.features[0].geometry.coordinates
+				    // Consulta de puntos del kilómetro
+				    var puntos_kilometro = ajax(`${$("#url").val()}/filtros/obtener`, {"tipo": "vias_geometria", "id":  {"id_sector": null, "id_via": incidente.id_via_configuracion, "kilometro": kilometro}}, 'JSON')
+				    
+				    // Si trae resultados
+				    if(puntos_kilometro.features.length > 0){
+				    	// Se almacena las coordenadas
+			       		let coordenadas_kilometro = puntos_kilometro.features[0].geometry.coordinates
 
-		       		// Se recorren las coordenadas
-		       		for (var i = 0; i < coordenadas_kilometro.length; i++){
-		       			// Se invierten para poder ser ubicadas geoespacialmente
-		                coordenadas.push(coordenadas_kilometro[i].reverse())
-		            }
+			       		// Se recorren las coordenadas
+			       		for (var i = 0; i < coordenadas_kilometro.length; i++){
+			       			// Se invierten para poder ser ubicadas geoespacialmente
+			                coordenadas.push(coordenadas_kilometro[i].reverse())
+			            }
 
-	            	// Se crea una polilínea y se obtienen las coordenadas
-		            var polilinea = new L.polyline(coordenadas)
+		            	// Se crea una polilínea y se obtienen las coordenadas
+			            var polilinea = new L.polyline(coordenadas)
 
-		            // Se obtienen las coordenadas de la polilínea
-		            var coordenadas_polilinea = polilinea.getLatLngs()
+			            // Se obtienen las coordenadas de la polilínea
+			            var coordenadas_polilinea = polilinea.getLatLngs()
 
-					// Se calcula el punto exacto
-					var coordenada = L.GeometryUtil.interpolateOnLine(mapa, coordenadas_polilinea, residuo_abscisa)
+						// Se calcula el punto exacto
+						var coordenada = L.GeometryUtil.interpolateOnLine(mapa, coordenadas_polilinea, residuo_abscisa)
 
-					// Se crea el marcador
-					var marcador = L.marker(coordenada.latLng)
-		                .on("click", function(){
-		                	// Modal con información
-		                    swal({
-		                      title: `Incidente en ${incidente.nombre_via_configuracion}`,
-		                      text: `
-		                        Abscisa: ${incidente.abscisa_real}
-		                        Informado por ${incidente.informado_por_nombre}
-		                        Fecha: ${incidente.fecha}
-		                        `,
-		                      buttons: false,
-		                      timer: 5000
-		                    })
-		                })
-		                .addTo(mapa)
-					
-					// Se hace zoom al kilómetro
-					mapa.fitBounds(polilinea.getBounds())
-			    }
+						// Se crea el marcador
+						var marcador = L.marker(coordenada.latLng)
+			                .on("click", function(){
+			                	// Modal con información
+			                    swal({
+			                      title: `Incidente en ${incidente.nombre_via_configuracion}`,
+			                      text: `
+			                        Abscisa: ${incidente.abscisa_real}
+			                        Informado por ${incidente.informado_por_nombre}
+			                        Fecha: ${incidente.fecha}
+			                        `,
+			                      buttons: false,
+			                      timer: 5000
+			                    })
+			                })
+			                .addTo(mapa)
+						
+						// Se hace zoom al kilómetro
+						mapa.fitBounds(polilinea.getBounds())
 
-			    // Al incidente se le insertan las coordenadas
-			    ajax(`${$("#url").val()}/operaciones/actualizar`, {"tipo": "incidente", "id": this.id_incidente, "datos": {"longitud": coordenada.latLng.lng, "latitud": coordenada.latLng.lat}}, 'JSON')
+						// Al incidente se le insertan las coordenadas
+					    ajax(`${$("#url").val()}/operaciones/actualizar`, {"tipo": "incidente", "id": this.id_incidente, "datos": {"longitud": coordenada.latLng.lng, "latitud": coordenada.latLng.lat}}, 'JSON')
 
-			    // Se retornan las coordenadas
-				return coordenada.latLng
+					    // Se retornan las coordenadas
+						return coordenada.latLng
+				    }
+				}
 			}
 		}
 
 		// Se invoca la clase y se ejecuta
 		const coordenadas = new Coordenadas(id_incidente)
 		coordenadas.obtener
+
+		/***********************************************************************************************************
+		 ************************* Script para generar coordenadas a incidentes existentes *************************
+		 ***********************************************************************************************************/
+		// var incidentes = ajax(`${$("#url").val()}/operaciones/obtener`, {"tipo": "incidentes", "id": {"id_sector": null, "id_via": null}}, 'JSON')
+		
+		// // Recorrido de las coordenadas
+		// $.each(incidentes, function(key, incidente){ 
+		// 	// Si el incidente no tiene coordenadas creadas
+		// 	if(!incidente.latitud){
+		// 		// Se ejecuta la clase creadora de las coordenadas
+		// 		const coordenadas = new Coordenadas(incidente.id)
+		// 		coordenadas.obtener
+				
+		// 		imprimir(coordenadas)
+		// 	}
+		// })
+		/***********************************************************************************************************
+		 ***********************************************************************************************************
+		 ***********************************************************************************************************/
 	})
 </script>
