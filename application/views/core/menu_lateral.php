f604c179bd2a83b339d127c3c5c7af5076da1a8b
John Arley <johnarleycano@hotmail.com>
2018-09-27 14:19:26 -0500

    style(Menú lateral): estilos actualizados

--- application/views/core/menu_lateral.php
+++ application/views/core/menu_lateral.php
@@ -2,42 +2,60 @@
     <div id="offcanvas-nav" uk-offcanvas="overlay: false; mode: push; flip: false;">
         <div id="menu_lateral" class="uk-offcanvas-bar">
             <ul class="uk-nav uk-nav-default">
-                <li class="uk-active"><a href="<?php echo site_url(''); ?>">INICIO</a></li>
+                <li class="uk-active">
+                    <a href="<?php echo site_url(''); ?>">
+                        <i class="fas fa-home fa-lg"></i>&nbsp;&nbsp;&nbsp;INICIO
+                    </a>
+                </li>
                 
-                <!-- <li class="uk-parent">
-                    <a href="#">REPORTES</a>
+                <li class="uk-parent">
+                    <a href="#">
+                        <i class="fas fa-car fa-lg"></i>&nbsp;&nbsp;&nbsp;OPERACIONES</li>
+                    </a>
+
                     <ul class="uk-nav-sub">
-                        <li><a href="#">Sub item</a></li>
-                        <li><a href="#">Sub item</a></li>
+                        <div class="uk-margin-small uk-grid-small uk-child-width-auto uk-grid">
+                            <label>
+                                <?php if(ENVIRONMENT == 'development'){ ?>
+                                    <input class="uk-checkbox" type="checkbox" checked>&nbsp;&nbsp;&nbsp;
+                                <?php } ?>
+                                
+                                <a href="<?php echo site_url('operaciones/eventos'); ?>" class="uk-link-text">Eventos diarios</a>
+                            </label>
+
+                            <label>
+                                <?php if(ENVIRONMENT == 'development'){ ?>
+                                    <input class="uk-checkbox" type="checkbox">&nbsp;&nbsp;&nbsp;
+                                <?php } ?>
+                                
+                                <a href="<?php echo site_url('operaciones/incidentes'); ?>">Incidentes</a>
+                            </label>
+                        </div>
                     </ul>
-                </li> -->
+                </li>
 
-                <li class="uk-nav-header">OPERACIONES</li>
-                <!-- <li><a href="<?php // echo site_url('operaciones/eventos'); ?>"><i class="far fa-calendar-alt fa-lg"></i>&nbsp;&nbsp;&nbsp;&nbsp;Eventos diarios</a></li> -->
-                <li><a href="<?php echo site_url('operaciones/incidentes'); ?>"><i class="fas fa-car fa-lg"></i>&nbsp;&nbsp;&nbsp;&nbsp;Incidentes</a></li>
-                
-                <li class="uk-nav-header">INVENTARIO</li>
-                <li><a href="<?php echo site_url('inventario/senales_verticales'); ?>"><i class="fas fa-truck fa-lg"></i>&nbsp;&nbsp;&nbsp;Señales verticales</a></li>
-                <!-- <li><a href="<?php // echo site_url('basculas/pesaje'); ?>"><i class="fas fa-truck fa-lg"></i>&nbsp;&nbsp;&nbsp;Señales horizontales</a></li> -->
-                <!-- <li><a href="<?php // echo site_url('basculas/pesaje'); ?>"><i class="fas fa-truck fa-lg"></i>&nbsp;&nbsp;&nbsp;Obras de arte</a></li> -->
-                
-                <?php if(false){ ?>
-                    <!-- Menús pendientes de habilitar -->
-                    <?php // if(ENVIRONMENT === 'development'){ ?>
-                        <li class="uk-nav-header">INVENTARIO</li>
-                        <li><a onCLick="javascript:medir_roceria()"><i class="far fa-paper-plane fa-lg"></i>&nbsp;&nbsp;&nbsp;&nbsp;Agregar</a></li>
-                        <li><a href="<?php // echo site_url('mediciones/ver'); ?>"><i class="fas fa-list-alt fa-lg"></i>&nbsp;&nbsp;&nbsp;&nbsp;Ver</a></li>
-
-                        <li class="uk-nav-header">MANTENIMIENTO</li>
-                        <li><a onCLick="javascript:medir_roceria()"><i class="far fa-paper-plane fa-lg"></i>&nbsp;&nbsp;&nbsp;&nbsp;Iniciar</a></li>
-                        <li><a href="<?php // echo site_url('mediciones/ver'); ?>"><i class="fas fa-list-alt fa-lg"></i>&nbsp;&nbsp;&nbsp;&nbsp;Ver</a></li>
-                    <?php // } ?>
-                <?php } ?>
+                <li class="uk-parent">
+                    <a href="#">
+                        <i class="fas fa-map fa-lg"></i>&nbsp;&nbsp;&nbsp;INVENTARIO</li>
+                    </a>
+                    
+                    <ul class="uk-nav-sub">
+                        <div class="uk-margin-small uk-grid-small uk-child-width-auto uk-grid">
+                            <label>
+                                <?php if(ENVIRONMENT == 'development'){ ?>
+                                    <input class="uk-checkbox" type="checkbox" checked>&nbsp;&nbsp;&nbsp;
+                                <?php } ?>
+                                
+                                <a href="<?php echo site_url('inventario/senales_verticales'); ?>" class="uk-link-text">Señales verticales</a>
+                            </label>
+                        </div>
+                    </ul>
+                </li>
                     
                 <!-- <li class="uk-nav-header"></li> -->
                 <li class="uk-nav-divider"></li>
-                <li><a href="<?php // echo site_url('reportes'); ?>"><i class="far fa-clipboard fa-lg"></i>&nbsp;&nbsp;&nbsp;&nbsp;Reportes</a></li>
-                <li><a href="<?php // echo site_url('configuracion'); ?>"><i class="fas fa-cog fa-lg"></i>&nbsp;&nbsp;&nbsp;Configuración</a></li>
+                <li><a href="#<?php // echo site_url('reportes'); ?>"><i class="far fa-clipboard fa-lg"></i>&nbsp;&nbsp;&nbsp;&nbsp;Reportes</a></li>
+                <li><a href="#<?php // echo site_url('configuracion'); ?>"><i class="fas fa-cog fa-lg"></i>&nbsp;&nbsp;&nbsp;Configuración</a></li>
                 
                 <li class="uk-nav-divider"></li>
                 <li><a href="<?php echo site_url('sesion/cerrar'); ?>"><i class="fas fa-sign-out-alt fa-lg"></i>&nbsp;&nbsp;&nbsp;Salir</a></li>
