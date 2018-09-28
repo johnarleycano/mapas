af2e76089ff7debd0a4e72b26161d77393f5c328
John Arley <johnarleycano@hotmail.com>
2018-09-28 10:54:00 -0500

    style(General): cambios en el menú

--- application/views/core/menu_lateral.php
+++ application/views/core/menu_lateral.php
@@ -8,6 +8,7 @@
                     </a>
                 </li>
                 
+                <li class="uk-nav-divider"></li>
                 <li class="uk-parent">
                     <a href="#">
                         <i class="fas fa-car fa-lg"></i>&nbsp;&nbsp;&nbsp;OPERACIONES</li>
@@ -68,13 +69,61 @@
                         </div>
                     </ul>
                 </li>
-                    
-                <!-- <li class="uk-nav-header"></li> -->
+
                 <li class="uk-nav-divider"></li>
-                <li><a href="#<?php // echo site_url('reportes'); ?>"><i class="far fa-clipboard fa-lg"></i>&nbsp;&nbsp;&nbsp;&nbsp;Reportes</a></li>
+                <li class="uk-parent">
+                    <a href="#">
+                        <i class="fas fa-map fa-lg"></i>&nbsp;&nbsp;&nbsp;MAPAS BASE</li>
+                    </a>
+
+                    <ul class="uk-nav-sub">
+                        <div class="uk-margin-small uk-grid-small uk-child-width-auto uk-grid">
+                            <?php if(ENVIRONMENT == 'development'){ ?>
+                                <label>
+                                    <input class="uk-radio" type="radio" name="radio2" checked> Bing
+                                </label>
+                                
+                                <label>
+                                    <input class="uk-radio" type="radio" name="radio2"> Open Street
+                                </label>
+                                
+                                <label>
+                                    <input class="uk-radio" type="radio" name="radio2"> Open Street (Gris)
+                                </label>
+                            <?php } ?>
+                        </div>
+                    </ul>
+                </li>
+
+                <li class="uk-parent">
+                    <a href="#">
+                        <i class="fas fa-map fa-lg"></i>&nbsp;&nbsp;&nbsp;CARTOGRAFÍA BASE</li>
+                    </a>
+
+                    <ul class="uk-nav-sub">
+                        <div class="uk-margin-small uk-grid-small uk-child-width-auto uk-grid">
+                            <?php if(ENVIRONMENT == 'development'){ ?>
+                                <label>
+                                    <input class="uk-checkbox" type="checkbox" checked>&nbsp;&nbsp;&nbsp;
+                                    <a href="<?php echo site_url('inventario/senales_verticales'); ?>" class="uk-link-text">Vías</a>
+                                </label>
+
+                                <label>
+                                    <input class="uk-checkbox" type="checkbox" checked>&nbsp;&nbsp;&nbsp;
+                                    <a href="<?php echo site_url('inventario/senales_verticales'); ?>" class="uk-link-text">Kilómetros</a>
+                                </label>
+
+                                <label>
+                                    <input class="uk-checkbox" type="checkbox" checked>&nbsp;&nbsp;&nbsp;
+                                    <a href="<?php echo site_url('inventario/senales_verticales'); ?>" class="uk-link-text">Abscisas</a>
+                                </label>
+                            <?php } ?>
+                        </div>
+                    </ul>
+                </li>
+
+                <li class="uk-nav-divider"></li>
                 <li><a href="#<?php // echo site_url('configuracion'); ?>"><i class="fas fa-cog fa-lg"></i>&nbsp;&nbsp;&nbsp;Configuración</a></li>
-                
-                <li class="uk-nav-divider"></li>
                 <li><a href="<?php echo site_url('sesion/cerrar'); ?>"><i class="fas fa-sign-out-alt fa-lg"></i>&nbsp;&nbsp;&nbsp;Salir</a></li>
             </ul>
         </div>
--- application/views/core/menu_superior.php
+++ application/views/core/menu_superior.php
@@ -6,7 +6,7 @@
     
 	<!-- Título del mapa -->
     <div class="uk-navbar-right" id="filtro_superior">
-    	<ul class="uk-navbar-nav">
+    	<ul class="uk-navbar-nav" id="titulo_mapa">
     		<li class="uk-active"><a href="#"><b><?php echo $titulo_mapa; ?></b></a></li>
     	</ul>
     </div>
